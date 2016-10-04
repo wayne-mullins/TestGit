0535113a980886279ec0670aeff9ffdfd771e0df
Wayne Mullins <wayne.mullins@mdnllc.net>
2016-10-04 18:36:07 -0400

    Commit Print Line #2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Test01.main()!");
+		System.out.println("Print Line #1");
+		System.out.println("Print Line #2");
 
 	}
 
