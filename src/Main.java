5bb2a1f308ebe0f19a5adb3bb0e54820d9e0d152
Wayne Mullins <wayne.mullins@mdnllc.net>
2016-10-07 15:50:10 -0400

    Commit line 14 from Eclipse

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Test01.main()!");
-		System.out.println("Print Line #1");
-		System.out.println("Print Line #2");
-		System.out.println("Print Line #3");
-		System.out.println("Print Line #5");
-		System.out.println("Print Line #6");
-		System.out.println("Print Line #7");		
-		System.out.println("Print Line #8");		
-		System.out.println("Manually added Print Line #8");		
-		System.out.println("Manually added Print Line #9");		
 		System.out.println("Pushed from WS2 #10");
 		System.out.println("Pushed from WS1 #11");
 		System.out.println("Pushed from WS1 #12");
+		System.out.println("Pushed from WS1 #12");
+		System.out.println("Pushed from WS1 #14");
 
 		
 	}
