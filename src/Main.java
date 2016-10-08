5c0900ab6f77aa79d5a883a6fce24f9fff8a7bca
R W Mullins <rmullin1@my.westga.edu>
2016-10-08 11:24:15 -0400

    Push line

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Delete all lines from home:Test1");
 		System.out.println("Add Line #1 from WS Test1");
+		System.out.println("Add Line #2 from WS Test2");
 	}
 
 }
