ebb3e85d56d7d3e43979198ecda7ff82a4bc8e71
R W Mullins <rmullin1@my.westga.edu>
2016-10-08 10:42:21 -0400

    Remove all lines using home:Test1

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is Test01.main()!");
-		System.out.println("Print Line #1");
-		System.out.println("Print Line #2");
-		System.out.println("Print Line #3");
-		System.out.println("Print Line #5");
-		System.out.println("Print Line #6");
-		System.out.println("Print Line #7");		
-		System.out.println("Print Line #8");		
-		System.out.println("Manually added Print Line #9");		
-		System.out.println("Pushed from WS2 #10");
-		System.out.println("Pushed from WS1 #11");
-		System.out.println("Pushed from WS1 #12");
-		System.out.println("Pushed from WS1 #12");
-		System.out.println("Pushed from WS1 #14");
-		System.out.println("Pushed from WS1 #15");
-		System.out.println("Pushed from WS1 #20");
-		System.out.println("Pushed from WS1 #21");
-		System.out.println("Pushed from WS1 #30");
-		System.out.println("Pushed from WS1 #31");
-		System.out.println("Manually Edited WS1 #32");
-		System.out.println("Manually Edited WS2 #33");
-		System.out.println("Manually Edited WS1 #34");
-		System.out.println("Manually Edited WS1 #35");
-		System.out.println("Manually Edited WS1 #36");
-		System.out.println("Manually Edited WS1 #37");
-				
+		System.out.println("Delete all lines from home:Test1");
 	}
 
 }
