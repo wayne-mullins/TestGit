5b57ad0b288d96ba02871b6a63b1fef54b60cbfe
R W Mullins <rmullin1@my.westga.edu>
2016-10-08 12:00:46 -0400

    Add line #3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Add Line #0 frm WS Test2");
 		System.out.println("Add Line #1 from WS Test1");
 		System.out.println("Add Line #2 from WS Test2");
+		System.out.println("Add Line #3 from WS Test2");
 	}
 
 }
